450f355f16e9e28e81170c8e63fc45528df471aa
webhero-nur <nurealom522@gmail.com>
2023-01-29 01:11:46 +0600

    Some question added

---
+++ qAa/question-answer.js
@@ -0,0 +1,11 @@
+/* 
+#Q1: What is Single Page Application (SPA) and Multy Page Application (MPA)?
+
+
+#Q2: Describe the advantages and disadvantages of SPA.
+
+
+#Q3: What are the difference between SPA and MPA?
+
+
+*/
