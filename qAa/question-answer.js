76788d2a1edd86c2872ac03165df876b294f2abc
webhero-nur <nurealom522@gmail.com>
2023-01-29 02:16:58 +0600

    Interview Question added

--- qAa/question-answer.js
+++ qAa/question-answer.js
@@ -1,4 +1,6 @@
 /* 
+// General Questions
+
 #Q1: Single Page Application (SPA) and Multy Page Application (MPA)?
 
 
@@ -9,5 +11,20 @@
 
 
 #Q4: HTML attribute vs JS Object properties.
+
+
+*/
+
+/* 
+// Interview Questions
+
+1. What are the components in react?
+
+*2. What do you mean by SPA?
+
+3. Examples of SPA, MPA
+
+*4. What is the best between SPA and MPA?
+
 
 */
